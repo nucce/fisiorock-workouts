abe16042e5bf59a8f26a87bba51e8483db1db1bc
nucce <paolo.denuccio@gmail.com>
2026-01-12 18:06:01 +0100

    modificato pop-up note. Reso modale

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,10 +3,9 @@ import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 
 export default function Note({note}) {
-    console.log(note);
     return (
         <Popup trigger={<span className="note-popup">📝</span>}
-               position="right center">
+               position="right center" modal={true}>
             <div className="note-popup-body">{note}</div>
         </Popup>
     )
